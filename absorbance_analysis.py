8dafff8cd31a56a8f3a30bbd8567c8bcbe3fc8f5
Junorium <johnrichard.perez.2004@gmail.com>
2024-10-07 19:17:41 -0400

    init absorbance workflow

---
+++ absorbance_analysis.py
@@ -0,0 +1,18 @@
+import nunmpy as np
+import pandas as pd
+
+# analyze treating background as particle
+     # requires a noise removal process
+# analyze treating large blots as particles
+
+# analyze each set of samples
+# add a tuple of the labeled concentration and the measured absorbance
+
+def background_analysis(picture):
+    return None
+
+def noise_removal(picture):
+    return None
+
+def particle_analysis(picture):
+    return None
